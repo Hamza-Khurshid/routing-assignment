09a636beba1f9c5564421d7971d0ff01e44561eb
Ali Raza <mirza.ali710@gmail.com>
2019-03-03 14:22:31 +0500

    Nested routes

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,10 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Route, Link} from 'react-router-dom';
+import TodayNews from './TodayNews';
+import PreviousNews from './PreviousNews';
 
 export default class News extends Component {
     render() {
         return (
             <div>
                 <h1>News</h1>
+                <div className='div1'>
+                    <Link to='/news/today'>Today</Link> <br />
+                    <Link to='/news/prev'>Previous</Link>
+                </div>
+
+                <div className='div2'>
+                    <Route path='/news/today' component={TodayNews} />
+                    <Route path='/news/prev' component={PreviousNews} />
+                </div>
             </div>
         )
     }
---
+++ src/components/TodayNews.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class TodayNews extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Today News</h1>
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum voluptate, natus nisi officiis harum nemo nulla rerum impedit repellat assumenda obcaecati minus accusamus iste soluta facere consectetur quod? Ratione, quasi! </p>
+            </div>
+        )
+    }
+};
